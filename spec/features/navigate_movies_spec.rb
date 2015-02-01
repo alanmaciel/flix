55d455794906392537b46102e35bc673caaa540a
alan m <alan@ubernetics.com>
2015-01-31 23:55:14 -0600

    Added feature to navigate from movies list to a show a movie details and viceversa.

---
+++ spec/features/navigate_movies_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "Navigating movies" do
+  it "Allows navigation from the detail page to the listing page" do
+    #Arrange
+    movie = Movie.create(movie_attributes)
+    
+    #Act
+    visit movie_url(movie)
+    
+    click_link "All Movies"
+    
+    #Assert
+    expect(current_path).to eq(movies_path)
+  end
+  
+  it "Allows navigation from the listing page to the detail page" do
+    #Arrange
+    movie = Movie.create(movie_attributes)
+    
+    #Act
+    visit movies_url
+    
+    click_link movie.title
+    
+    #Assert
+    expect(current_path).to eq(movie_path(movie))
+  end
+
+end
+
--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -4,11 +4,7 @@ describe "Viewing an individual movie" do
 
   it "show movie's details" do
   #Arrange
-  movie = Movie.create(title: "Iron-Man",
-                       rating:  "PG-13",
-                       total_gross: 318412101.00,
-                       description: "Tony Stark builds an armored suit to fight the throes of evil.",
-                       released_on: "2008-05-02")
+  movie = Movie.create(movie_attributes(total_gross: 300000000.00))
   #Act
   visit movie_url(movie)
 
@@ -17,7 +13,7 @@ describe "Viewing an individual movie" do
   expect(page).to have_text(movie.rating)
   expect(page).to have_text(movie.description)
   expect(page).to have_text(movie.released_on)
-  expect(page).to have_text("$318,412,101.00")
+  expect(page).to have_text("$300,000,000.00")
 
   end
 end
