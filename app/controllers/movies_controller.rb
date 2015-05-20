03554f7254c50fc1c181aae5f8e9358df3accc2b
Alan Maciel <alan@ubernetics.com>
2015-05-20 15:03:36 -0500

    Added Feature Spec, Route and Action for Updating a Movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,16 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    # this one doesn't raise an exception 
+    # movie_params = params[:movie].permit(:title, :description, :rating, :released_on, :total_gross)
+    # this one is if you want to permit all attributes:
+    # movie_params = params.require(:movie).permit!
+    # this one raises exceptions
+    movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
 end
