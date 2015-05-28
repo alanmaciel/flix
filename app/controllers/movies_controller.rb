d429732876df4b81ba29d672040e3ec798895ffe
Alan Maciel <alan@ubernetics.com>
2015-05-27 20:39:00 -0500

    Deleting a movie, adding link and action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -30,7 +30,14 @@ class MoviesController < ApplicationController
     redirect_to @movie
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
 private
+
   def movie_params
     params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
   end
