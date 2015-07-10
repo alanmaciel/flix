e4c632958314eb0da4253c837bf511ec170e6d02
Alan Maciel <alan@ubernetics.com>
2015-07-09 23:41:33 -0500

    updating assets

--- spec/features/navigate_movies_spec.rb
+++ spec/features/navigate_movies_spec.rb
@@ -1,31 +1,26 @@
-require_relative '../spec_helper'
+require 'spec_helper'
 
 describe "Navigating movies" do
-  it "Allows navigation from the detail page to the listing page" do
-    #Arrange
+  
+  it "allows navigation from the detail page to the listing page" do
+
     movie = Movie.create(movie_attributes)
-    
-    #Act
+
     visit movie_url(movie)
-    
+
     click_link "All Movies"
-    
-    #Assert
+
     expect(current_path).to eq(movies_path)
   end
   
-  it "Allows navigation from the listing page to the detail page" do
-    #Arrange
+  it "allows navigation from the listing page to the detail page" do
+
     movie = Movie.create(movie_attributes)
-    
-    #Act
+
     visit movies_url
-    
+
     click_link movie.title
-    
-    #Assert
+
     expect(current_path).to eq(movie_path(movie))
   end
-
 end
-
