f02739389740b178b1d0444e72f1d17143463593
Alan Maciel <alan@ubernetics.com>
2015-07-19 20:38:58 -0500

    adding Location to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ActiveRecord::Base
 
   validates :name, presence: true
   validates :comment, length: { minimum: 4 }
-
+  
   STARS = [ 1, 2, 3, 4, 5 ]
   
   validates :stars, inclusion: { 
